2052ecc97cc7ef80e1aed90578c8eacb2e6b7660
airstrider <airstrider@gmail.com>
2023-10-09 15:32:21 -0400

    rollback unfinished works

---
+++ webapp/linear_app.py
@@ -0,0 +1,44 @@
+from flask_restful import reqparse
+from flask import Flask, render_template, request
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import joblib
+import json
+
+
+modelfile = Path(__file__).parent / \
+    '../data/simple_linear_regression_model.pkl'
+model = joblib.load(modelfile)
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template('simple.html')
+
+
+@app.route('/simple/', methods=['POST'])
+def simple():
+    if request.is_json:
+        dict = request.get_json(silent=True)
+        df = pd.DataFrame(dict)
+
+        prediction = model.predict(df)
+        results = prediction.to_dict()
+        return results, 200
+    else:
+        radio = request.form['radio_budget']
+        dict = {'Radio': [radio]}
+        df = pd.DataFrame(dict, dtype=float)
+
+        prediction = model.predict(df)
+        df['sales'] = prediction
+        df = df.rename(columns={'Radio': 'promo_radio'})
+        results = df.to_html(index=False, justify='center', border=0)
+        return render_template('simple_after.html', output=results)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9090)
