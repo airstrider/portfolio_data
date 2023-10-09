554fae506f5a3557ba6eab36116678ba10d7f966
airstrider <airstrider@gmail.com>
2023-10-09 17:23:34 -0400

    format codes

--- webapp/linear_app.py
+++ webapp/linear_app.py
@@ -7,10 +7,9 @@ import pandas as pd
 import joblib
 import json
 
-
-modelfile = Path(__file__).parent / \
-    '../data/simple_linear_regression_model.pkl'
+modelfile = Path(__file__).parent/'../data/simple_linear_regression_model.pkl'
 model = joblib.load(modelfile)
+
 app = Flask(__name__)
 
 
@@ -22,6 +21,7 @@ def main():
 @app.route('/simple/', methods=['POST'])
 def simple():
     if request.is_json:
+        # REST API (JSON)
         dict = request.get_json(silent=True)
         df = pd.DataFrame(dict)
 
@@ -29,14 +29,15 @@ def simple():
         results = prediction.to_dict()
         return results, 200
     else:
+        # HTTP (HTML)
         radio = request.form['radio_budget']
         dict = {'Radio': [radio]}
         df = pd.DataFrame(dict, dtype=float)
 
         prediction = model.predict(df)
         df['sales'] = prediction
-        df = df.rename(columns={'Radio': 'promo_radio'})
-        results = df.to_html(index=False, justify='center', border=0)
+        df = df.rename(columns={'Radio': 'budget'})
+        results = df.to_html(index=False, justify='left', border=0)
         return render_template('simple_after.html', output=results)
 
 
