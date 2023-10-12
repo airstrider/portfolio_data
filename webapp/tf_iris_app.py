0a9e1079afe626e92f57d8ffdf4cdb880b132a6f
airstrider <airstrider@gmail.com>
2023-10-12 01:15:20 -0400

    update business logic rename html files fix typos

--- webapp/tf_iris_app.py
+++ webapp/tf_iris_app.py
@@ -14,33 +14,55 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('simple_linear_req.html')
+    return render_template('tf_iris_req.html')
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    json_req = request.get_json(silent=True)
+    if request.is_json:
+        # REST API (JSON)
+        json_req = request.get_json(silent=True)
 
-    sepal_len = json_req['sepal_length']
-    sepal_wid = json_req['sepal_width']
-    petal_len = json_req['petal_length']
-    petal_wid = json_req['petal_width']
+        sepal_len = json_req['sepal_length']
+        sepal_wid = json_req['sepal_width']
+        petal_len = json_req['petal_length']
+        petal_wid = json_req['petal_width']
 
-    req = [[sepal_len, sepal_wid,
-            petal_len, petal_wid]]
+        req = [[sepal_len, sepal_wid,
+                petal_len, petal_wid]]
 
-    # predict - return ndarray
-    prediction = model.predict(req)
+        # predict - return ndarray
+        prediction = model.predict(req)
 
-    # result
-    result = prediction[0].astype('str')
-    result = dict(list(enumerate(result, start=0)))
-    category = max(result, key=result.get)
-    value = get_label(category)
-    res = {'category': category, 'value': value}
+        # result
+        result = prediction[0].astype('str')
+        result = dict(list(enumerate(result, start=0)))
+        category = max(result, key=result.get)
+        value = get_label(category)
+        res = {'category': category, 'value': value}
 
-    # REST test
-    return res, 200
+        # REST test
+        return res, 200
+    else:
+        # HTTP (HTML)
+        sepal_len = float(request.form['sepal_length'])
+        sepal_wid = float(request.form['sepal_width'])
+        petal_len = float(request.form['petal_length'])
+        petal_wid = float(request.form['petal_width'])
+
+        req = [[sepal_len, sepal_wid,
+                petal_len, petal_wid]]
+
+        # predict - return ndarray
+        prediction = model.predict(req)
+
+        # result
+        result = prediction[0].astype('str')
+        result = dict(list(enumerate(result, start=0)))
+        category = max(result, key=result.get)
+        res = get_label(category)
+
+        return render_template('tf_iris_res.html', data=res)
 
 
 def get_label(label_num):
